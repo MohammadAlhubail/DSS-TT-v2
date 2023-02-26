9162c9d0e556ccf5185a7fc151d89209f38bdad4
hubailmn <76243856+hubailmn@users.noreply.github.com>
2023-02-26 19:47:29 +0900

    Beta

--- JavaScript/signup.js
+++ JavaScript/signup.js
@@ -2,7 +2,7 @@
 function googleLogin() {
   var clientId =
     "649303250093-2bnvclv310ser66hhla42vf4uo1s2kbc.apps.googleusercontent.com";
-  var redirectUri = "https://mohammadalhubail.github.io/DSS-TT-v2/index.html";
+  var redirectUri = "https://mohammadalhubail.github.io/DSS-TT-v2/index";
   var url =
     "https://accounts.google.com/o/oauth2/auth?client_id=" +
     clientId +
